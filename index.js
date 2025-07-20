a1438d4e1da5f03e28f36d9962b21384017c06cb
nabbonoushad <nabbonoushad@gmail.com>
2025-07-21 01:06:08 +0600

    added "process.env.port ||" so it runs better on prod servers

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = 7320
+const port = process.env.port || 7320
 app.use(cors())
 let state = {
     ME:{
